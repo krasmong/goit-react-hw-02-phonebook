a16a71d409d9dc4561a8cccf795513d3ca9e10ee
Oleksandr Pomalin <nilamopr@gmail.com>
2021-03-10 18:04:48 +0200

    Добавляет компонент "ContactForm"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
-import "./App.css";
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello my lovely</h1>
-    </div>
-  );
+import './App.css';
+
+class App extends Component {
+  // formSubmitHendler = (data) => {
+  //   console.log(data);
+  //   const myContacts = {
+
+  //     name: data.name,
+  //     number: data.number,
+  //   };
+  // };
+
+  // ==========================  ContactForm ====================
+
+  // static defaultProps = {
+  //   name: '',
+  //   number: '',
+  // };
+
+  // static PropTypes = {
+  //   contacts: PropTypes.array
+  //   name: PropTypes.string,
+  //   number: PropTypes.string,
+  // };
+
+  state = {
+    contacts: [],
+    name: '',
+  };
+  nameInputId = uuidv4();
+  numberInputId = uuidv4();
+
+  handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+    // console.log(`handleChange: ${this.state.name}`);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(`handleSubmit: ${this.state.name}`);
+    this.props.OnSaveContacts(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  // ==========================  ContactForm ====================
+
+  render() {
+    return (
+      <>
+        <h1 className="title">Phonebook</h1>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor={this.nameInputId}>
+              Name
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                id={this.nameInputId}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
